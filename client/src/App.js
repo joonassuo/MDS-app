0712d4dee4b1a6c770e53bb6cabc0639b6617e09
joonassuo <joonas.suonpera@gmail.com>
2020-03-18 20:24:44 +0200

    added boilerplate for redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<h1>Hello</h1>
+			</div>
+		</Provider>
 	);
 }
 
