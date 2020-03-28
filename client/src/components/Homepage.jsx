deb0fcbea92144bb7ad0f4ef876cd0485367f789
joonassuo <joonas.suonpera@gmail.com>
2020-03-28 18:10:07 +0200

    added add offer button and some styling

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import OfferCard from "./OfferCard";
 import { useSelector } from "react-redux";
 import Navbar from "./Navbar";
+import "./css/Homepage.css";
 
 const Homepage = () => {
 	const offerList = useSelector(state => state.offer.offers);
@@ -11,9 +12,12 @@ const Homepage = () => {
 		return (
 			<div>
 				<Navbar />
-				{offerList.map(offer => (
-					<OfferCard key={offer.id} offer={offer} />
-				))}
+				<div className="cards-container">
+					{offerList.map(offer => (
+						<OfferCard key={offer.id} offer={offer} />
+					))}
+				</div>
+				<img src="/add.png" alt="plus" id="add=button" />
 			</div>
 		);
 	}
