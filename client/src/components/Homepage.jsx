338e2bc32e4e9103f9aeda6ee7c3df4cdc4b978a
joonassuo <joonas.suonpera@gmail.com>
2020-04-29 15:35:05 +0300

    addded carousel and started working on styling

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import OfferCard from "./OfferCard";
 import { useSelector } from "react-redux";
+import Carousel from "react-bootstrap/Carousel";
 import Navbar from "./Navbar";
 import Addoffer from "./AddOffer";
 import "./css/Homepage.css";
@@ -15,23 +16,45 @@ const Homepage = () => {
 		<div>
 			<Navbar user={user} auth={isAuth} />
 			{isAdd ? <Addoffer toggleAdd={setIsAdd} /> : null}
-			<div>
-				{offerList ? (
-					<div className="cards-container">
-						{offerList
-							.filter((offer) => !offer.isActive)
-							.map((offer) => (
-								<OfferCard offer={offer} key={offer._id} />
-							))}
-					</div>
-				) : null}
-				<img
-					src="/add.png"
-					alt="plus"
-					id="add-button"
-					onClick={() => setIsAdd(true)}
-				/>
+			<div className="navigation-bar">
+				<div>Browse</div>
+				<div>Active</div>
+				<div>My offers</div>
 			</div>
+			<Carousel classname="carousel" interval={null}>
+				<Carousel.Item>
+					<div>
+						{offerList ? (
+							<div className="cards-container">
+								{offerList
+									.filter((offer) => !offer.isActive)
+									.map((offer) => (
+										<OfferCard
+											offer={offer}
+											key={offer._id}
+										/>
+									))}
+							</div>
+						) : null}
+						<img
+							src="/add.png"
+							alt="plus"
+							id="add-button"
+							onClick={() => setIsAdd(true)}
+						/>
+					</div>
+				</Carousel.Item>
+				<Carousel.Item>
+					<div>
+						<h1>slide2</h1>
+					</div>
+				</Carousel.Item>
+				<Carousel.Item>
+					<div>
+						<h1>slide2</h1>
+					</div>
+				</Carousel.Item>
+			</Carousel>
 		</div>
 	);
 };
