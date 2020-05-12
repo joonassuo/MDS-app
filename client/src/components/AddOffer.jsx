a59fe8d3bebe0b1ea9cf6c3020287bc0da3e350d
joonassuo <joonas.suonpera@gmail.com>
2020-05-12 13:15:29 +0300

    added level and duration sliders

--- client/src/components/AddOffer.jsx
+++ client/src/components/AddOffer.jsx
@@ -1,144 +1,148 @@
 import React, { useState } from "react";
 import { addOffer, getOffers } from "../actions/offerActions";
 import { useDispatch, useSelector } from "react-redux";
-import Slider from "@material-ui/core/Slider";
 import "./css/addOffer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Redirect } from "react-router-dom";
 
 const AddOffer = (props) => {
-  const [title, setTitle] = useState("");
-  const [cost, setCost] = useState(0);
-  const [duration, setDuration] = useState(30);
-  const [description, setDescription] = useState("");
-  const [level, setLevel] = useState(0);
-  const [ready, setReady] = useState(false);
-  const user = useSelector((state) => state.auth.user);
-  const dispatch = useDispatch();
+	const [title, setTitle] = useState("");
+	const [cost, setCost] = useState(0);
+	const [duration, setDuration] = useState(30);
+	const [description, setDescription] = useState("");
+	const [level, setLevel] = useState(0);
+	const [ready, setReady] = useState(false);
+	const user = useSelector((state) => state.auth.user);
+	const dispatch = useDispatch();
 
-  const dur = (type) => {
-    const sign = type === "increase" ? 1 : -1;
-    if (sign < 0 && duration > 15) {
-      setDuration(duration - 15);
-    } else if (sign > 0 && duration < 240) {
-      setDuration(duration + 15);
-    }
-  };
+	const durationValue = (duration) => {
+		const hours = Math.floor(duration / 60);
+		const minutes = duration % 60;
+		return hours + ":" + minutes + (minutes < 10 ? "0" : "") + " h";
+	};
 
-  const durationValue = (duration) => {
-    const hours = Math.floor(duration / 60);
-    const minutes = duration % 60;
-    return hours + ":" + minutes + (minutes < 10 ? "0" : "") + " h";
-  };
+	const onChange = (e, type) => {
+		switch (type) {
+			case "level":
+				setLevel(e.target.value);
+				break;
+			case "cost":
+				setCost(e.target.value);
+				break;
+			case "title":
+				setTitle(e.target.value);
+				break;
+			case "description":
+				setDescription(e.target.value);
+				break;
+			case "duration":
+				setDuration(e.target.value);
+				break;
+			default:
+				return;
+		}
+	};
 
-  const onChange = (e, type) => {
-    switch (type) {
-      case "level":
-        console.log(e.target);
-        //setLevel(e.target.aria-valuenow);
-        break;
-      case "cost":
-        setCost(e.target.value);
-        break;
-      case "title":
-        setTitle(e.target.value);
-        break;
-      case "description":
-        setDescription(e.target.value);
-        break;
-      default:
-        return;
-    }
-  };
+	const handleSubmit = () => {
+		// Get current user from state
+		const creator = {
+			id: user._id,
+			username: user.username,
+			karma: user.karma,
+			profile_picture: user.profile_picture,
+		};
 
-  const handleSubmit = () => {
-    // Get current user from state
-    const creator = {
-      id: user._id,
-      username: user.username,
-      karma: user.karma,
-      profile_picture: user.profile_picture,
-    };
+		// Create expires date a week from now
+		Date.prototype.addDays = function (days) {
+			var date = new Date(this.valueOf());
+			date.setDate(date.getDate() + days);
+			return date;
+		};
+		var date = new Date();
+		const expiresDate = date.addDays(7);
 
-    // Create expires date a week from now
-    Date.prototype.addDays = function (days) {
-      var date = new Date(this.valueOf());
-      date.setDate(date.getDate() + days);
-      return date;
-    };
-    var date = new Date();
-    const expiresDate = date.addDays(7);
+		const offer = {
+			creator,
+			cost,
+			level,
+			expiresDate,
+			description,
+			duration,
+			title,
+		};
 
-    const offer = {
-      creator,
-      cost,
-      level,
-      expiresDate,
-      description,
-      duration,
-      title,
-    };
+		addOffer(offer)(dispatch);
+		getOffers()(dispatch);
+		setReady(true);
+	};
 
-    addOffer(offer)(dispatch);
-    getOffers()(dispatch);
-    setReady(true);
-  };
+	// Get level label based on slider value
+	const getLevel = (level) => {
+		const levels = ["Beginner", "Intermediate", "Advanced", "Expert"];
+		return levels[level];
+	};
 
-  return ready ? (
-    <Redirect to="/" />
-  ) : (
-    <div>
-      <div className="background" onClick={() => props.toggleAdd(false)}></div>
-      <div className="addoffer-container">
-        <div className="header">ADD NEW OFFER</div>
-        <input
-          type="text"
-          placeholder="TITLE"
-          id="title-field"
-          onChange={(e) => onChange(e, "title")}
-        />
-        <textarea
-          rows="6"
-          cols="30"
-          placeholder="Give a brief description about your offering"
-          id="description-field"
-          onChange={(e) => onChange(e, "description")}
-        />
-        <h2>{level}</h2>
-        <Slider
-          onChangeCommitted={(e) => onChange(e, "level")}
-          aria-valuetext="value"
-          defaultValue={3}
-          step={1}
-          marks
-          min={0}
-          max={5}
-          valueLabelDisplay="auto"
-        />
-        <div id="input-duration">
-          <div id="label-duration">Duration</div>
-          <button id="duration-minus" onClick={() => dur("decrease")}>
-            -
-          </button>
-          <div id="duration-value">{durationValue(duration)}</div>
-          <button id="duration-plus" onClick={() => dur("increase")}>
-            +
-          </button>
-        </div>
-        <input
-          type="number"
-          name="cost"
-          id="cost-field"
-          className="center"
-          placeholder="PRICE"
-          onChange={(e) => onChange(e, "cost")}
-        />
-        <button id="submit-button" onClick={() => handleSubmit()}>
-          SUBMIT
-        </button>
-      </div>
-    </div>
-  );
+	return ready ? (
+		<Redirect to="/" />
+	) : (
+		<div>
+			<div
+				className="background"
+				onClick={() => props.toggleAdd(false)}
+			></div>
+			<div className="addoffer-container">
+				<div className="header">ADD NEW OFFER</div>
+				<input
+					type="text"
+					placeholder="TITLE"
+					id="title-field"
+					onChange={(e) => onChange(e, "title")}
+				/>
+				<textarea
+					rows="6"
+					cols="30"
+					placeholder="Give a brief description about your offering"
+					id="description-field"
+					onChange={(e) => onChange(e, "description")}
+				/>
+				<div className="level-container">
+					<div id="level-label">LEVEL</div>
+					<div id="level">{getLevel(level)}</div>
+					<input
+						type="range"
+						min="0"
+						max="3"
+						className="slider"
+						value={level}
+						onChange={(e) => onChange(e, "level")}
+					/>
+				</div>
+				<div className="duration-container">
+					<div id="duration-label">DURATION</div>
+					<div id="duration">{durationValue(duration)}</div>
+					<input
+						type="range"
+						min="0"
+						max="240"
+						step="15"
+						className="slider"
+						onChange={(e) => onChange(e, "duration")}
+					/>
+				</div>
+				<input
+					type="number"
+					name="cost"
+					id="cost-field"
+					className="center"
+					placeholder="PRICE"
+					onChange={(e) => onChange(e, "cost")}
+				/>
+				<button id="submit-button" onClick={() => handleSubmit()}>
+					SUBMIT
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default AddOffer;
