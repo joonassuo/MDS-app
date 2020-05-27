fa2a32c9e8dd4a93dc6be55973fc1589d6376de5
joonassuo <joonas.suonpera@gmail.com>
2020-05-27 18:40:16 +0300

    added styling for login and register

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -8,208 +8,225 @@ import "./css/Homepage.css";
 import { modifyOffer, getOffers } from "../actions/offerActions";
 import { getUser, modifyUser } from "../actions/userActions";
 import { loadUser } from "../actions/authActions";
+import { Redirect } from "react-router-dom";
 
 const Homepage = () => {
-	const [isAdd, setIsAdd] = useState(false);
-	const [showCompletePopup, setShowCompletePopup] = useState(false);
-	const [toComplete, setToComplete] = useState({});
-	const dispatch = useDispatch();
-	const isAuth = useSelector((state) => state.auth.isAuthenticated);
-	const offerList = useSelector((state) => state.offer.offers);
-	const user = useSelector((state) => state.auth.user);
+  const [isAdd, setIsAdd] = useState(false);
+  const [showCompletePopup, setShowCompletePopup] = useState(false);
+  const [toComplete, setToComplete] = useState({});
+  const [isLogin, setIsLogin] = useState(false);
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const offerList = useSelector((state) => state.offer.offers);
+  const user = useSelector((state) => state.auth.user);
 
-	// OFFER COMPLETION SCREEN POPUP
-	const CompletePopup = () => {
-		const [rating, setRating] = useState(0);
+  // OFFER COMPLETION SCREEN POPUP
+  const CompletePopup = () => {
+    const [rating, setRating] = useState(0);
 
-		// Get buyer or seller
-		const isCreator = user._id === toComplete.creator.id ? true : false;
+    // Get buyer or seller
+    const isCreator = user._id === toComplete.creator.id ? true : false;
 
-		// HANDLE OFFER COMPLETION EVENT
-		const handleCompletion = () => {
-			if (rating === 0) {
-				alert("Please enter rating");
-				return;
-			}
+    // HANDLE OFFER COMPLETION EVENT
+    const handleCompletion = () => {
+      if (rating === 0) {
+        alert("Please enter rating");
+        return;
+      }
 
-			// PUT req bodies
-			const offerBody = JSON.stringify({
-				isActive: false,
-				isCompleted: true,
-				buyerRating: isCreator ? true : false,
-				creatorRating: isCreator ? false : true,
-			});
-			const userBody = JSON.stringify({
-				karma: user.karma + rating * 2,
-			});
-			const id = isCreator ? toComplete.buyer.id : toComplete.creator.id;
+      // PUT req bodies
+      const offerBody = JSON.stringify({
+        isActive: false,
+        isCompleted: true,
+        buyerRating: isCreator ? true : false,
+        creatorRating: isCreator ? false : true,
+      });
+      const userBody = JSON.stringify({
+        karma: user.karma + rating * 2,
+      });
+      const id = isCreator ? toComplete.buyer.id : toComplete.creator.id;
 
-			modifyOffer(toComplete._id, offerBody);
-			modifyUser(id, userBody);
-			getOffers()(dispatch);
-			getUser(user._id)(dispatch);
-			setShowCompletePopup(false);
-		};
+      modifyOffer(toComplete._id, offerBody);
+      modifyUser(id, userBody);
+      getOffers()(dispatch);
+      getUser(user._id)(dispatch);
+      setShowCompletePopup(false);
+    };
 
-		const pic = isCreator
-			? toComplete.buyer.profile_picture
-			: toComplete.creator.profile_picture;
+    const pic = isCreator
+      ? toComplete.buyer.profile_picture
+      : toComplete.creator.profile_picture;
 
-		return (
-			<div>
-				<div
-					className="complete-offer-background"
-					onClick={() => {
-						console.log("click");
-						setShowCompletePopup(false);
-					}}
-				></div>
-				<div className="complete-offer-container">
-					<div id="complete-title">
-						{toComplete.title.toUpperCase()}
-					</div>
-					<div id="complete-pic-container">
-						<img
-							id="complete-pic"
-							src={pic ? pic : "/default.png"}
-							alt="profile-pic"
-						/>
-					</div>
-					<div>
-						Please Rate{" "}
-						{isCreator
-							? toComplete.buyer.username.toUpperCase()
-							: toComplete.creator.username.toUpperCase()}
-					</div>
-					<div id="complete-star-container">
-						<img
-							className="complete-star"
-							src={rating > 0 ? "/star_full.png" : "/star.png"}
-							alt="star"
-							onClick={() => setRating(1)}
-						/>
-						<img
-							className="complete-star"
-							src={rating > 1 ? "/star_full.png" : "/star.png"}
-							alt="star"
-							onClick={() => setRating(2)}
-						/>
-						<img
-							className="complete-star"
-							src={rating > 2 ? "/star_full.png" : "/star.png"}
-							alt="star"
-							onClick={() => setRating(3)}
-						/>
-					</div>
-					<div
-						id="complete-submit"
-						onClick={() => handleCompletion()}
-					>
-						SUBMIT
-					</div>
-					<div
-						id="complete-cancel"
-						onClick={() => setShowCompletePopup(false)}
-					>
-						CANCEL
-					</div>
-				</div>
-			</div>
-		);
-	};
+    return (
+      <div>
+        <div
+          className="complete-offer-background"
+          onClick={() => {
+            setShowCompletePopup(false);
+          }}
+        ></div>
+        <div className="complete-offer-container">
+          <div id="complete-title">{toComplete.title.toUpperCase()}</div>
+          <div id="complete-pic-container">
+            <img
+              id="complete-pic"
+              src={pic ? pic : "/default.png"}
+              alt="profile-pic"
+            />
+          </div>
+          <div>
+            Please Rate{" "}
+            {isCreator
+              ? toComplete.buyer.username.toUpperCase()
+              : toComplete.creator.username.toUpperCase()}
+          </div>
+          <div id="complete-star-container">
+            <img
+              className="complete-star"
+              src={rating > 0 ? "/star_full.png" : "/star.png"}
+              alt="star"
+              onClick={() => setRating(1)}
+            />
+            <img
+              className="complete-star"
+              src={rating > 1 ? "/star_full.png" : "/star.png"}
+              alt="star"
+              onClick={() => setRating(2)}
+            />
+            <img
+              className="complete-star"
+              src={rating > 2 ? "/star_full.png" : "/star.png"}
+              alt="star"
+              onClick={() => setRating(3)}
+            />
+          </div>
+          <div id="complete-submit" onClick={() => handleCompletion()}>
+            SUBMIT
+          </div>
+          <div id="complete-cancel" onClick={() => setShowCompletePopup(false)}>
+            CANCEL
+          </div>
+        </div>
+      </div>
+    );
+  };
 
-	return (
-		<div>
-			<Navbar user={user} auth={isAuth} />
-			{isAdd ? <Addoffer toggleAdd={setIsAdd} /> : null}
-			{showCompletePopup ? <CompletePopup /> : null}
-			<div className="navigation-bar">
-				<div>Browse</div>
-				<div>Active</div>
-				<div>My offers</div>
-			</div>
-			<Carousel classname="carousel" interval={null}>
-				{/*
+  return isLogin ? (
+    <Redirect to="/login" />
+  ) : (
+    <div>
+      <Navbar user={user} auth={isAuth} />
+      {isAdd ? <Addoffer toggleAdd={setIsAdd} /> : null}
+      {showCompletePopup ? <CompletePopup /> : null}
+      <div className="navigation-bar">
+        <div>Browse</div>
+        <div>Active</div>
+        <div>My offers</div>
+      </div>
+      <Carousel classname="carousel" interval={null}>
+        {/*
 				--------------BROWSE ALL OFFERS---------------
 				*/}
-				<Carousel.Item id="carousel-browse">
-					<div>
-						{offerList ? (
-							<div className="cards-container">
-								{offerList
-									.filter(
-										(offer) =>
-											!offer.isActive &&
-											!offer.isCompleted
-									)
-									.map((offer) => (
-										<OfferCard
-											offer={offer}
-											key={offer._id}
-										/>
-									))}
-							</div>
-						) : null}
-						<img
-							src="/add.png"
-							alt="plus"
-							id="add-button"
-							onClick={() => setIsAdd(true)}
-						/>
-					</div>
-				</Carousel.Item>
-				{/*
+        <Carousel.Item id="carousel-browse">
+          <div>
+            {offerList ? (
+              <div className="cards-container">
+                {offerList
+                  .filter((offer) => !offer.isActive && !offer.isCompleted)
+                  .map((offer) => (
+                    <OfferCard offer={offer} key={offer._id} />
+                  ))}
+              </div>
+            ) : null}
+            <img
+              src="/add.png"
+              alt="plus"
+              id="add-button"
+              onClick={() => setIsAdd(true)}
+            />
+          </div>
+        </Carousel.Item>
+        {/*
 				-------------BROWSE ACTIVE OFFERS--------------
 				*/}
-				<Carousel.Item id="carousel-active">
-					<div>
-						{offerList && user ? (
-							<div className="cards-container">
-								{offerList
-									.filter(
-										(offer) =>
-											(offer.creator.id === user._id ||
-												offer.buyer.id === user._id) &&
-											offer.isActive
-									)
-									.map((offer) => (
-										<OfferCard
-											offer={offer}
-											key={offer._id}
-											toggleShow={setShowCompletePopup}
-											toComplete={setToComplete}
-											activeOffer={true}
-										/>
-									))}
-							</div>
-						) : null}
-					</div>
-				</Carousel.Item>
-				{/*
+        <Carousel.Item id="carousel-active">
+          {user ? (
+            <div>
+              {offerList && user ? (
+                <div className="cards-container">
+                  {offerList
+                    .filter(
+                      (offer) =>
+                        (offer.creator.id === user._id ||
+                          offer.buyer.id === user._id) &&
+                        offer.isActive
+                    )
+                    .map((offer) => (
+                      <OfferCard
+                        offer={offer}
+                        key={offer._id}
+                        toggleShow={setShowCompletePopup}
+                        toComplete={setToComplete}
+                        activeOffer={true}
+                      />
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          ) : (
+            <div className="carousel-login-container">
+              <div className="carousel-login-text">
+                Login to view your content
+              </div>
+              <div
+                className="carousel-login-button"
+                onClick={() => setIsLogin(true)}
+              >
+                LOGIN
+              </div>
+            </div>
+          )}
+        </Carousel.Item>
+        {/*
 				--------------BROWSE YOUR OFFERS--------------
 				*/}
-				<Carousel.Item id="carousel-my-offers">
-					<div>
-						{offerList && user ? (
-							<div className="cards-container">
-								{offerList
-									.filter(
-										(offer) => (offer.creator.id === user._id) && !offer.isActive
-									)
-									.map((offer) => (
-										<OfferCard
-											offer={offer}
-											key={offer._id}
-											myOffers={true}
-										/>
-									))}
-							</div>
-						) : null}
-					</div>
-				</Carousel.Item>
-			</Carousel>
-		</div>
-	);
+        <Carousel.Item id="carousel-my-offers">
+          {user ? (
+            <div>
+              {offerList && user ? (
+                <div className="cards-container">
+                  {offerList
+                    .filter(
+                      (offer) =>
+                        offer.creator.id === user._id && !offer.isActive
+                    )
+                    .map((offer) => (
+                      <OfferCard
+                        offer={offer}
+                        key={offer._id}
+                        myOffers={true}
+                      />
+                    ))}
+                </div>
+              ) : null}
+            </div>
+          ) : (
+            <div className="carousel-login-container">
+              <div className="carousel-login-text">
+                Login to view your content
+              </div>
+              <div
+                className="carousel-login-button"
+                onClick={() => setIsLogin(true)}
+              >
+                LOGIN
+              </div>
+            </div>
+          )}
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Homepage;
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,141 +5,166 @@ import { Redirect } from "react-router-dom";
 import "./css/login.css";
 
 const Login = () => {
-	const [firstname, setFirstname] = useState("");
-	const [lastname, setLastname] = useState("");
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [confirm, setConfirm] = useState("");
-	const [isRegister, setIsRegister] = useState(false);
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const dispatch = useDispatch();
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const dispatch = useDispatch();
 
-	const onChange = (e, type) => {
-		e.preventDefault();
+  const onChange = (e, type) => {
+    e.preventDefault();
 
-		switch (type) {
-			case "firstname":
-				setFirstname(e.target.value);
-				break;
-			case "lastname":
-				setLastname(e.target.value);
-				break;
-			case "username":
-				setUsername(e.target.value);
-				break;
-			case "email":
-				setEmail(e.target.value);
-				break;
-			case "password":
-				setPassword(e.target.value);
-				break;
-			case "confirm-password":
-				setConfirm(e.target.value);
-				break;
-			default:
-				return;
-		}
-	};
+    switch (type) {
+      case "firstname":
+        setFirstname(e.target.value);
+        break;
+      case "lastname":
+        setLastname(e.target.value);
+        break;
+      case "username":
+        setUsername(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
+      case "confirm-password":
+        setConfirm(e.target.value);
+        break;
+      default:
+        return;
+    }
+  };
 
-	const onSubmit = () => {
-		// Login or register new user
-		if (!isRegister) {
-			const user = {
-				email,
-				password,
-			};
-			loginUser(user)(dispatch);
-			setIsLoggedIn(true);
-		} else {
-			// Check if password confirmation matches
-			if (password !== confirm) {
-				alert("passwords don't match");
-			} else {
-				const user = {
-					firstname,
-					lastname,
-					username,
-					email,
-					password,
-				};
-				registerUser(user)(dispatch);
-				setIsLoggedIn(true);
-			}
-		}
-	};
+  const onSubmit = () => {
+    // Login or register new user
+    if (!isRegister) {
+      const user = {
+        email,
+        password,
+      };
+      loginUser(user)(dispatch);
+      setIsLoggedIn(true);
+    } else {
+      // Check if password confirmation matches
+      if (password !== confirm) {
+        alert("passwords don't match");
+      } else {
+        const user = {
+          firstname,
+          lastname,
+          username,
+          email,
+          password,
+        };
+        registerUser(user)(dispatch);
+        setIsLoggedIn(true);
+      }
+    }
+  };
 
-	return isLoggedIn ? (
-		<Redirect to="/" />
-	) : (
-		<div>
-			<div className="login-container">
-				{isRegister ? (
-					<div>
-						<input
-							type="text"
-							className="firstname-field h-align"
-							placeholder="First Name"
-							onChange={(e) => onChange(e, "firstname")}
-						/>
-						<input
-							type="text"
-							className="lastname-field h-align"
-							placeholder="Last Name"
-							onChange={(e) => onChange(e, "lastname")}
-						/>
-						<input
-							type="text"
-							className="username-field h-align"
-							placeholder="Username"
-							onChange={(e) => onChange(e, "username")}
-						/>
-					</div>
-				) : null}
-				<div className="hive-login-container">
-					<a href="https://api.intra.42.fr/oauth/authorize?client_id=8f7dbe7ac964071bad261bdc3197b8c40b26a2bc5105046c3245ab2635a28ecb&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2F42%2Fredirect&response_type=code&scope=public">
-						Login with hive
-					</a>
-				</div>
-				<input
-					className="email-field h-align"
-					type="email"
-					placeholder="Email"
-					onChange={(e) => onChange(e, "email")}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					className="pasword-field h-align"
-					onChange={(e) => onChange(e, "password")}
-				/>
-				{isRegister ? (
-					<div>
-						<input
-							type="password"
-							placeholder="Confirm Password"
-							className="confirm-pasword-field h-align"
-							onChange={(e) => onChange(e, "confirm-password")}
-						/>
-					</div>
-				) : null}
-				<button className="submit-button h-align" onClick={onSubmit}>
-					{isRegister ? <div>REGISTER</div> : <div>LOGIN</div>}
-				</button>
-				{!isRegister ? (
-					<div className="register-link">
-						<div
-							onClick={() => {
-								setIsRegister(true);
-							}}
-						>
-							Don't have an account yet?
-						</div>
-					</div>
-				) : null}
-			</div>
-		</div>
-	);
+  const handleExit = () => {
+    if (isRegister) {
+      setIsRegister(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  };
+
+  return isLoggedIn ? (
+    <Redirect to="/" />
+  ) : (
+    <div className="login-screen">
+      <img
+        src={isRegister ? "/back.png" : "/cancel.png"}
+        alt="cancel"
+        className="back-button"
+        onClick={() => handleExit()}
+      />
+      <div className="login-container">
+        {isRegister ? (
+          <div>
+            <input
+              type="text"
+              className="firstname-field h-align"
+              placeholder="First Name"
+              onChange={(e) => onChange(e, "firstname")}
+            />
+            <input
+              type="text"
+              className="lastname-field h-align"
+              placeholder="Last Name"
+              onChange={(e) => onChange(e, "lastname")}
+            />
+            <input
+              type="text"
+              className="username-field h-align"
+              placeholder="Username"
+              onChange={(e) => onChange(e, "username")}
+            />
+          </div>
+        ) : (
+          <a
+            id="hive-login-link"
+            href="https://api.intra.42.fr/oauth/authorize?client_id=8f7dbe7ac964071bad261bdc3197b8c40b26a2bc5105046c3245ab2635a28ecb&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2F42%2Fredirect&response_type=code&scope=public"
+          >
+            <div className="hive-login-container h-align">
+              <img id="hive-login-logo" src="/hive.png" alt="hive-logo" />
+              <div id="hive-login-text">Hive Credentials</div>
+            </div>
+          </a>
+        )}
+        <input
+          className="email-field h-align"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => onChange(e, "email")}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="pasword-field h-align"
+          onChange={(e) => onChange(e, "password")}
+        />
+        {isRegister ? (
+          <div>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="confirm-pasword-field h-align"
+              onChange={(e) => onChange(e, "confirm-password")}
+            />
+          </div>
+        ) : null}
+        {isRegister ? (
+          <div className="register-button" onClick={onSubmit}>
+            REGISTER
+          </div>
+        ) : (
+          <div className="login-button" onClick={onSubmit}>
+            LOGIN
+          </div>
+        )}
+        {!isRegister ? (
+          <div className="register-link">
+            <div
+              onClick={() => {
+                setIsRegister(true);
+              }}
+            >
+              Don't have an account yet?
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
