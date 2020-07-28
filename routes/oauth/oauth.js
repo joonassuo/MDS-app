1266d34cff9b434116baca58943e5d6c07da783e
Joonas <joonas.suonpera@gmail.com>
2020-07-28 12:07:54 +0300

    heroku

--- routes/oauth/oauth.js
+++ routes/oauth/oauth.js
@@ -4,27 +4,27 @@ const axios = require("axios");
 const c = require("config");
 
 router.get("/42/redirect", (req, res) => {
-	const code = req.headers.code;
-	const body = JSON.stringify({
-		grant_type: "authorization_code",
-		client_id: c.get("CLIENT_ID"),
-		client_secret: c.get("CLIENT_SECRET"),
-		code,
-		redirect_uri: "http://localhost:3000/oauth/42/redirect",
-		scope: "public"
-	});
-	const config = {
-		headers: {
-			"Content-type": "application/json"
-		}
-	};
+  const code = req.headers.code;
+  const body = JSON.stringify({
+    grant_type: "authorization_code",
+    client_id: c.get("CLIENT_ID"),
+    client_secret: c.get("CLIENT_SECRET"),
+    code,
+    redirect_uri: "https://beeswax.herokuapp.com/oauth/42/redirect",
+    scope: "public",
+  });
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
 
-	axios
-		.post("https://api.intra.42.fr/oauth/token", body, config)
-		.then(response => {
-			res.send(response.data);
-		})
-		.catch(err => res.send(err));
+  axios
+    .post("https://api.intra.42.fr/oauth/token", body, config)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
